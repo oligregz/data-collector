273bf6d3c718a73e8fc7ea1a0d6438f8a3c893cf
oligregz <gregorysak90@gmail.com>
2023-11-27 21:16:59 -0300

    [Data Collector] - Starting Project :rocket:

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ res: 'Hello World!' });
+});
+
+module.exports = {
+  routes
+}
