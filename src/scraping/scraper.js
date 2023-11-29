e4b51f4ed34201081392579ce93db5ae33f966dc
oligregz <gregorysak90@gmail.com>
2023-11-29 16:04:18 -0300

    feat: add web scraping

---
+++ src/scraping/scraper.js
@@ -0,0 +1,49 @@
+const puppeteer = require('puppeteer');
+
+const extractDataFromPage = async (page) => {
+  await page.waitForSelector('.col-md-4.col-xl-4.col-lg-4');
+
+  const extractedData = await page.evaluate(() => {
+    const divSelector = '.col-md-4.col-xl-4.col-lg-4';
+    const divs = Array.from(document.querySelectorAll(divSelector));
+
+    const extractedInfo = divs.map((div) => {
+      const title = div.querySelector('a.title').textContent.trim();
+      const description = div.querySelector('p.description').textContent.trim();
+      const price = div.querySelector('h4.price').textContent.trim();
+
+      return {
+        title,
+        description,
+        price,
+      };
+    });
+
+    return extractedInfo;
+  });
+
+  return extractedData;
+};
+
+const searchAndListDetails = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const extractedData = await extractDataFromPage(page);
+
+  console.log('Detalhes de cada div:');
+  extractedData.forEach((data, index) => {
+    console.log(`Div ${index + 1}:`);
+    console.log('Nome:', data.title);
+    console.log('Descrição:', data.description);
+    console.log('Preço:', data.price);
+    console.log('-------------------------');
+  });
+
+  await browser.close();
+};
+
+searchAndListDetails();
