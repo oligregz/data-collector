267856c5bd05fec8a4ad80d59013fba4f030bd64
oligregz <gregorysak90@gmail.com>
2023-11-28 21:57:52 -0300

    feat: add list notebooks method

--- src/controllers/notebook.controller.js
+++ src/controllers/notebook.controller.js
@@ -1,6 +1,23 @@
 const { Notebook } = require('../models/Notebook');
 
 module.exports = {
+
+  async index(req, res) {
+    try {
+      const notebooks = await Notebook.findAll();
+
+      const hasNotebooks = !!notebooks && notebooks.length > 0;
+  
+      if (hasNotebooks) {
+        return res.status(200).json(notebooks);
+      }
+
+    } catch (e) {
+      console.error(e);
+      return res.staus(400).json({ error: e.parent.sqlMessage });
+    }
+  },
+
   async store(req, res) {
     try {
       const {
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ const routes = express.Router();
 
 const NotebookController = require('./controllers/notebook.controller');
 
-routes.post('/notebook', NotebookController.store);
+routes.post('/notebooks', NotebookController.store);
+routes.get('/notebooks', NotebookController.index);
+
 
 module.exports = {
   routes
