26ff3537d765eb98675fe3a632badab7e76eec92
oligregz <gregorysak90@gmail.com>
2023-12-07 14:56:33 -0300

    test: add two case tests for scraping layer

--- src/tests/unitTests/__tests__/scraping/scraping.spec.js
+++ src/tests/unitTests/__tests__/scraping/scraping.spec.js
@@ -1,6 +1,29 @@
+const {
+  searchAndListDetails,
+} = require('../../../../scraping/scraper');
+
+const tenSeconds = 10000;
+let result;
+
+beforeAll(async () => {
+  result = await searchAndListDetails();
+})
+
 describe('Scraping tests:', () => {
-  test('Ok Jest', () => {
-    expect(2 + 2).toBe(4);
-  })
+  test('1 - Return type', async () => {
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toBeGreaterThan(0);
+  }, tenSeconds);
+
+  test('2 - Check properties of items', async () => {
+    const firstItem = result[0];
+    expect(firstItem).toHaveProperty('name');
+    expect(firstItem).toHaveProperty('price');
+    expect(firstItem).toHaveProperty('description');
+  });
+
 });
 
+afterAll(() => {
+  
+});
